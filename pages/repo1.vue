a3bdc64aa2058bb66bcac641e1bf5396426fda5a
mibressler <53796824+mibressler@users.noreply.github.com>
2024-11-11 23:43:46 +0100

    Update repo1.vue

--- pages/repo1.vue
+++ pages/repo1.vue
@@ -31,31 +31,9 @@ definePageMeta({
   </div>
 
   <!-- Right Sidebar -->
-  <div class="hidden lg:block lg:w-2/5">
+  <div class="hidden lg:block lg:w-1/5">
     <!-- Right sidebar content here -->
-    <div class="bg-slate-50 rounded-2xl mt-10">
-    <div class="flex justify-start p-8">
-
-      
-  <UAvatar
-      size="xs"
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Deutscher_Bundestag_logo.svg/800px-Deutscher_Bundestag_logo.svg.png"
-      alt="Avatar"
-    />
-  <div class="mx-4">Deutscher Bundestag</div>
-  
-  
-  
-</div>
-
-
-
-  <div class="px-8">
-    <div class="font-semibold pb-2">About</div>
-    <div class="text-md">With the rapid development of artificial intelligence technology, large language models (LLMs) have made significant progress in fields such as natural language processing, computer vision, and scientific tasks. However, as the scale of these models increases, optimizing resource consumption while maintaining high performance has become a key challenge. </div>
-  </div>
-
-  <div class="px-8 pt-5 font-semibold text-blue-600"><a href="google.com">projectwebsite.org</a></div>
+    
 
 
   <div></div>
@@ -63,7 +41,7 @@ definePageMeta({
 
 </div>
   </div>
-</div>
+
 
 
 
