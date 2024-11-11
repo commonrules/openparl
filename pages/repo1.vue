54be660e697423d28d1f2ab21e9d2bf576d45240
mibressler <53796824+mibressler@users.noreply.github.com>
2024-11-11 23:42:10 +0100

    Update repo1.vue

--- pages/repo1.vue
+++ pages/repo1.vue
@@ -33,7 +33,7 @@ definePageMeta({
   <!-- Right Sidebar -->
   <div class="hidden lg:block lg:w-2/5">
     <!-- Right sidebar content here -->
-    <div class="bg-slate-50 rounded-lg mt-10">
+    <div class="bg-slate-50 rounded-2xl mt-10">
     <div class="flex justify-start p-8">
 
       
@@ -48,12 +48,14 @@ definePageMeta({
   
 </div>
 
-<div class="px-8 pb-5 font-semibold text-blue-600"><a href="google.com">projectwebsite.org</a></div>
+
 
   <div class="px-8">
-    <div class="font-semibold">Model Introduction</div>
+    <div class="font-semibold pb-2">About</div>
     <div class="text-md">With the rapid development of artificial intelligence technology, large language models (LLMs) have made significant progress in fields such as natural language processing, computer vision, and scientific tasks. However, as the scale of these models increases, optimizing resource consumption while maintaining high performance has become a key challenge. </div>
   </div>
+
+  <div class="px-8 pt-5 font-semibold text-blue-600"><a href="google.com">projectwebsite.org</a></div>
 
 
   <div></div>
