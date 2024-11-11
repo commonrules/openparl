a74108e9c7a6dd609198386a1fd2469c968f39ab
mibressler <53796824+mibressler@users.noreply.github.com>
2024-11-12 00:10:24 +0100

    Update repo1.vue

--- pages/repo1.vue
+++ pages/repo1.vue
@@ -26,7 +26,7 @@ definePageMeta({
   <!-- Center Content -->
   <div class="w-full xl:w-3/5 px-4">
     <!-- Main center content here -->
-    <div class="border border-slate-100 rounded-2xl mt-5">
+    <div class="border-slate-100 rounded-2xl mt-5">
       <div class="flex justify-start bg-slate-100 pl-5 py-4 rounded-tl-2xl rounded-tr-2xl"> 
         <UAvatar
       size="2xs"
