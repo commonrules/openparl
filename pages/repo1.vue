c6a048922017264db5cfe6f17196a24482ec5f77
mibressler <53796824+mibressler@users.noreply.github.com>
2024-11-11 16:38:05 +0100

    up

--- pages/repo1.vue
+++ pages/repo1.vue
@@ -43,7 +43,33 @@ class="pl-8 no-border mt-0 pt-0 pb-5 bg-slate-50"
 />
 
 
-<UHorizontalNavigation :links="links"  class="pl-5 bg-slate-50"/>
+<UHorizontalNavigation :ui="{
+  wrapper: 'relative w-full flex items-center justify-between',
+  container: 'flex items-center min-w-0',
+  inner: 'min-w-0',
+  base: 'group relative w-full flex items-center gap-1.5 px-2.5 py-3.5 rounded-md font-medium text-sm focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 disabled:cursor-not-allowed disabled:opacity-75',
+  before: 'before:absolute before:inset-x-0 before:inset-y-2 before:inset-px before:rounded-md hover:before:bg-gray-50 dark:hover:before:bg-gray-800/50',
+  after: 'after:absolute after:bottom-0 after:inset-x-2.5 after:block after:h-[2px] after:mt-2',
+  active: 'text-gray-900 dark:text-white after:bg-gradient-to-r after:from-gray-900 after:from-10% after:via-red-500 after:to-yellow-400 after: dark:after:bg-primary-400 after:rounded-full',
+  inactive: 'text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white',
+  label: 'truncate relative',
+  icon: {
+    base: 'flex-shrink-0 w-5 h-5 relative',
+    active: 'text-gray-700 dark:text-gray-200',
+    inactive: 'text-gray-400 dark:text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-200'
+  },
+  avatar: {
+    base: 'flex-shrink-0',
+    size: '2xs'
+  },
+  badge: {
+    base: 'flex-shrink-0 ms-auto relative rounded',
+    color: 'gray',
+    variant: 'solid',
+    size: 'xs'
+  }
+}
+" :links="links"  class="pl-5 bg-slate-50"/>
 
 </template>
 
@@ -51,4 +77,5 @@ class="pl-8 no-border mt-0 pt-0 pb-5 bg-slate-50"
 .no-border {
   border-bottom: none;
 }
+
 </style>
