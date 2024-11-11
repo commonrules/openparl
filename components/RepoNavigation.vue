f4c5b762cc9d75eb639df30a7d1a5db8dd29c96e
mibressler <53796824+mibressler@users.noreply.github.com>
2024-11-11 21:15:58 +0100

    wad

---
+++ components/RepoNavigation.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+
+
+const links = [
+  [{
+  label: 'Gesetzestext',
+  icon: 'i-heroicons-book-open',
+  to: '/repo1'
+}, {
+  label: 'Pull Requests',
+  icon: 'i-heroicons-square-3-stack-3d',
+  to: '/pro'
+}, {
+  label: 'Discussion',
+  icon: 'i-heroicons-rocket-launch',
+  to: '/releases'
+},{
+  label: 'Legislative Activity',
+  icon: 'i-heroicons-rocket-launch',
+  to: '/releases'
+}], [{
+    label: 'Open in Patina',
+    labelClass: 'bg-gray-600 text-white pr-3 pl-3 pt-1 pb-1 rounded-md mb-0 mt-0',
+  }, {
+    icon: 'i-heroicons-heart'
+  },{
+    icon: 'i-heroicons-ellipsis-vertical'
+   }]
+]
+
+</script>
+
+
+<template>
+<UPageHeader :ui="{
+    wrapper: 'relative pt-2',
+    container: 'flex flex-col lg:flex-row lg:items-center lg:justify-between',
+    headline: 'mb-3 text-sm/6 font-semibold text-primary flex items-center gap-1.5',
+    title: 'text-2xl sm:text-3xl font-normal text-gray-900 dark:text-white tracking-tight',
+    description: 'mt-4 text-lg text-gray-500 dark:text-gray-400',
+    icon: {
+      wrapper: 'flex',
+      base: 'w-10 h-10 flex-shrink-0 text-primary'
+    },
+    links: 'flex flex-wrap items-center gap-1.5 mt-4 lg:mt-0'
+  
+  }"
+  title="Grundgesetz für die Bundesrepublik Deutschland"
+  class="pl-8 no-border mt-0 pt-0 pb-5 bg-slate-50"
+  />
+  
+  
+  <UHorizontalNavigation :ui="{
+    wrapper: 'relative w-full flex items-center justify-between',
+    container: 'flex items-center min-w-0',
+    inner: 'min-w-0',
+    base: 'group relative w-full flex items-center gap-1.5 px-2.5 py-3.5 rounded-md font-medium text-sm focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 disabled:cursor-not-allowed disabled:opacity-75',
+    before: 'before:absolute before:inset-x-0 before:inset-y-2 before:inset-px before:rounded-md hover:before:bg-gray-50 dark:hover:before:bg-gray-800/50',
+    after: 'after:absolute after:bottom-0 after:inset-x-2.5 after:block after:h-[2px] after:mt-2',
+    active: 'text-gray-900 dark:text-white after:bg-[linear-gradient(266deg,rgba(252,211,77,1)0%,rgba(252,211,77,1)33%,rgba(220,38,38,1)33%,rgba(220,38,38,1)66%,rgba(10,10,10,1)66%,rgba(10,10,10,1)100%)] dark:after:bg-primary-400 after:rounded-full',
+    inactive: 'text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white',
+    label: 'truncate relative',
+    icon: {
+      base: 'flex-shrink-0 w-5 h-5 relative',
+      active: 'text-gray-700 dark:text-gray-200',
+      inactive: 'text-gray-400 dark:text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-200'
+    },
+    avatar: {
+      base: 'flex-shrink-0',
+      size: '2xs'
+    },
+    badge: {
+      base: 'flex-shrink-0 ms-auto relative rounded',
+      color: 'gray',
+      variant: 'solid',
+      size: 'xs'
+    }
+  }
+  " :links="links"  class="pl-5 bg-slate-50"/>
+  </template>
+
+<style scoped>
+.no-border {
+  border-bottom: none;
+}
+
+</style>
--- pages/repo1.vue
+++ pages/repo1.vue
@@ -3,325 +3,38 @@ definePageMeta({
   layout: 'repo'
 })
 
-const links = [
-  [{
-  label: 'Gesetzestext',
-  icon: 'i-heroicons-book-open',
-  to: '/repo1'
-}, {
-  label: 'Pull Requests',
-  icon: 'i-heroicons-square-3-stack-3d',
-  to: '/pro'
-}, {
-  label: 'Discussion',
-  icon: 'i-heroicons-rocket-launch',
-  to: '/releases'
-},{
-  label: 'Legislative Activity',
-  icon: 'i-heroicons-rocket-launch',
-  to: '/releases'
-}], [{
-    label: 'Open in Patina',
-    labelClass: 'bg-gray-600 text-white pr-3 pl-3 pt-1 pb-1 rounded-md mb-0 mt-0',
-  }, {
-    icon: 'i-heroicons-heart'
-  },{
-    icon: 'i-heroicons-ellipsis-vertical'
-   }]
-]
 
 </script>
 
 <template>
 
-<UPageHeader :ui="{
-  wrapper: 'relative pt-2',
-  container: 'flex flex-col lg:flex-row lg:items-center lg:justify-between',
-  headline: 'mb-3 text-sm/6 font-semibold text-primary flex items-center gap-1.5',
-  title: 'text-2xl sm:text-3xl font-normal text-gray-900 dark:text-white tracking-tight',
-  description: 'mt-4 text-lg text-gray-500 dark:text-gray-400',
-  icon: {
-    wrapper: 'flex',
-    base: 'w-10 h-10 flex-shrink-0 text-primary'
-  },
-  links: 'flex flex-wrap items-center gap-1.5 mt-4 lg:mt-0'
-
-}"
-title="Grundgesetz für die Bundesrepublik Deutschland"
-class="pl-8 no-border mt-0 pt-0 pb-5 bg-slate-50"
-/>
-
-
-<UHorizontalNavigation :ui="{
-  wrapper: 'relative w-full flex items-center justify-between',
-  container: 'flex items-center min-w-0',
-  inner: 'min-w-0',
-  base: 'group relative w-full flex items-center gap-1.5 px-2.5 py-3.5 rounded-md font-medium text-sm focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 disabled:cursor-not-allowed disabled:opacity-75',
-  before: 'before:absolute before:inset-x-0 before:inset-y-2 before:inset-px before:rounded-md hover:before:bg-gray-50 dark:hover:before:bg-gray-800/50',
-  after: 'after:absolute after:bottom-0 after:inset-x-2.5 after:block after:h-[2px] after:mt-2',
-  active: 'text-gray-900 dark:text-white after:bg-[linear-gradient(266deg,rgba(252,211,77,1)0%,rgba(252,211,77,1)33%,rgba(220,38,38,1)33%,rgba(220,38,38,1)66%,rgba(10,10,10,1)66%,rgba(10,10,10,1)100%)] dark:after:bg-primary-400 after:rounded-full',
-  inactive: 'text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white',
-  label: 'truncate relative',
-  icon: {
-    base: 'flex-shrink-0 w-5 h-5 relative',
-    active: 'text-gray-700 dark:text-gray-200',
-    inactive: 'text-gray-400 dark:text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-200'
-  },
-  avatar: {
-    base: 'flex-shrink-0',
-    size: '2xs'
-  },
-  badge: {
-    base: 'flex-shrink-0 ms-auto relative rounded',
-    color: 'gray',
-    variant: 'solid',
-    size: 'xs'
-  }
-}
-" :links="links"  class="pl-5 bg-slate-50"/>
-
-
-<div class="pl-6 pt-8 pr-6" >
-
-  <table >
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 1</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Sie zu achten und zu schützen ist Verpflichtung aller staatlicen Gewalt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Das Deutsche Volk bekennt sich darum zu unverletzlichen und unveräußerlichen Menschenrechten als Grundlage jeder menschlichen Gemeinschaft, des Friedens und der Gerechtigkeit in der Welt.</td>
-  </tr>
-
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(3)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die nachfolgenden Grundrechte binden Gesetzgebung, vollziehende Gewalt und Rechtsprechung als unmittelbar geltendes Recht.</td>
-  </tr>
-</table>
-<br>
-<table>
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 2</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit, soweit er nicht die Rechte anderer verletzt und nicht gegen die verfassungsmäßige Ordnung oder das Sittengesetz verstößt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Die Freiheit der Person ist unverletzlich.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">³</span></td>
-    <td>In diese Rechte darf nur auf Grund eines Gesetzes eingegriffen werden..</td>
-  </tr>
-</table>
-<br>
-<table>
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 2</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit, soweit er nicht die Rechte anderer verletzt und nicht gegen die verfassungsmäßige Ordnung oder das Sittengesetz verstößt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Die Freiheit der Person ist unverletzlich.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">³</span></td>
-    <td>In diese Rechte darf nur auf Grund eines Gesetzes eingegriffen werden..</td>
-  </tr>
-</table>
-<br>
-<table>
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 2</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit, soweit er nicht die Rechte anderer verletzt und nicht gegen die verfassungsmäßige Ordnung oder das Sittengesetz verstößt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Die Freiheit der Person ist unverletzlich.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">³</span></td>
-    <td>In diese Rechte darf nur auf Grund eines Gesetzes eingegriffen werden..</td>
-  </tr>
-</table>
-<br>
-<table>
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 2</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit, soweit er nicht die Rechte anderer verletzt und nicht gegen die verfassungsmäßige Ordnung oder das Sittengesetz verstößt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Die Freiheit der Person ist unverletzlich.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">³</span></td>
-    <td>In diese Rechte darf nur auf Grund eines Gesetzes eingegriffen werden..</td>
-  </tr>
-</table>
-<br>
-<table>
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 2</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit, soweit er nicht die Rechte anderer verletzt und nicht gegen die verfassungsmäßige Ordnung oder das Sittengesetz verstößt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Die Freiheit der Person ist unverletzlich.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">³</span></td>
-    <td>In diese Rechte darf nur auf Grund eines Gesetzes eingegriffen werden..</td>
-  </tr>
-</table>
-<br>
-<table>
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 2</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit, soweit er nicht die Rechte anderer verletzt und nicht gegen die verfassungsmäßige Ordnung oder das Sittengesetz verstößt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Die Freiheit der Person ist unverletzlich.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">³</span></td>
-    <td>In diese Rechte darf nur auf Grund eines Gesetzes eingegriffen werden..</td>
-  </tr>
-</table>
-<br>
-<table>
-  <tr>
-    <td colspan="3"><span class="font-bold text-gray-700">Art. 2</span><span class="text-gray-500 "> Menschenwürde; Menschenrrechte; Grundrechtsbindung</span> </td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td class="pr-5">(1)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit, soweit er nicht die Rechte anderer verletzt und nicht gegen die verfassungsmäßige Ordnung oder das Sittengesetz verstößt.</td>
-  </tr>
-  <tr style="height:10px"><td colspan="5"></td></tr>
-  <tr>
-    <td class="pr-5">(2)</td>
-    <td><span class="text-gray-400">¹</span></td>
-    <td>Die Würde des Menschen ist unantastbar.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">²</span></td>
-    <td>Die Freiheit der Person ist unverletzlich.</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><span class="text-gray-400">³</span></td>
-    <td>In diese Rechte darf nur auf Grund eines Gesetzes eingegriffen werden..</td>
-  </tr>
-</table>
 
 
 
+<div class="flex justify-center">
 
 </div>
 
+<RepoNavigation />
+<div class="flex justify-center">
+  <!-- Left Sidebar -->
+  <div class="w-1/4 lg:w-1/5 hidden md:block"> <!-- Adjust width as needed -->
+    <!-- Left sidebar content here -->
+     
+  </div>
+
+  <!-- Center Content -->
+  <div class="w-full md:w-2/4 lg:w-3/5 px-4">
+    <!-- Main center content here -->
+
+    <GesetzesText />
+  </div>
+
+  <!-- Right Sidebar -->
+  <div class="w-1/4 lg:w-1/5 hidden md:block">
+    <!-- Right sidebar content here -->
+  </div>
+</div>
 
 </template>
 
