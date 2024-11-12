99ef78c761978680b6528690a8f108bc7dd539da
mibressler <53796824+mibressler@users.noreply.github.com>
2024-11-12 17:30:09 +0100

    Update Terminal.vue

--- components/Terminal.vue
+++ components/Terminal.vue
@@ -7,6 +7,7 @@
     :empty-state="null"
     placeholder="Crtl K to search, issue commands (find Norms) or ask model"
     class="bg-slate-50 dark:bg-gray-800"
+    :close-button="{ icon: 'i-heroicons-arrow-right-end-on-rectangle', color: 'gray', variant: 'link', padded: false }"
   />
   </div>
 </template>
