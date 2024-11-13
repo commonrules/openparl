bdc45a969c25f91a7a44f752a1da3b9bc1ce1cdf
mibressler <53796824+mibressler@users.noreply.github.com>
2024-11-13 20:40:32 +0100

    Update JurisDictionDrop.vue

--- components/JurisDictionDrop.vue
+++ components/JurisDictionDrop.vue
@@ -25,22 +25,6 @@ const people = [
   id: 'passau',
   label: 'City of Passau',
   avatar: { src: 'https://www.waidler.com/files/57/223957OR647__uct3.jpg?v=1641988982' }
-}, {
-  id: 'Atinux',
-  label: 'Atinux',
-  href: 'https://github.com/Atinux',
-  target: '_blank',
-  avatar: { src: 'https://avatars.githubusercontent.com/u/904724?v=4' }
-}, {
-  id: 'smarroufin',
-  label: 'smarroufin',
-  href: 'https://github.com/smarroufin',
-  target: '_blank',
-  avatar: { src: 'https://avatars.githubusercontent.com/u/7547335?v=4' }
-}, {
-  id: 'nobody',
-  label: 'Nobody',
-  icon: 'i-heroicons-user-circle'
 }]
 
 const selected = ref(people[0])
