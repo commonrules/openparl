3cf3f08729909e5a451e2e1dbba2631f9b42eb8e
mibressler <53796824+mibressler@users.noreply.github.com>
2024-12-17 04:07:20 +0100

    Update Logo.vue

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,14 +1,14 @@
 
 <template>
   <ULink to="/">
-    <div class="flex items-center">
+    <div class="">
       <img
         src="https://www.baywidi.de/wp-content/uploads/2020/05/cropped-BayWiDI_ICON_A3-1.png"
         alt="Avatar"
         width="30"
         height="30"
       />
-      <div class="text-xl rounded-lg ml-5">
+      <div class="text-xl rounded-lg mt-6 text-slate-500">
         BayWiDI Rechtsprechungsdatenbank
       </div>
     </div>
